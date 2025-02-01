7ed8b3ee24be2929d4a94c9ef680eaa0fc360d1d
GlaucoInfinities <162525681+GlaucoInfinities@users.noreply.github.com>
2025-02-01 12:09:45 +1000

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
-//export const WEBSOCKET_SERVER = "wss://api-staging.mjproapps.com:8081";
-export const WEBSOCKET_SERVER = "wss://api.mjproapps.com:8081";
+export const WEBSOCKET_SERVER = "wss://api-staging.mjproapps.com:8081";
+//export const WEBSOCKET_SERVER = "wss://api.mjproapps.com:8081";
 
 //export const SERVER = "http://127.0.0.1:8000/";
-//export const SERVER = "https://api-staging.mjproapps.com/";
-export const SERVER = "https://api.mjproapps.com/";
+export const SERVER = "https://api-staging.mjproapps.com/";
+//export const SERVER = "https://api.mjproapps.com/";
 
 export const SENDER_BOT = 'bot';
 export const SENDER_USER = 'user';
